84321b8b001f302403eccea215f0ac27ad859901
chiccomask <chiccomask@chiccomask-MBP.homenet.telecomitalia.it>
2017-10-13 11:31:48 +0200

    Second commit

--- www/js/index.js
+++ www/js/index.js
@@ -226,5 +226,5 @@ function disableButtons(disabled){
   document.querySelectorAll('#menu BUTTON').forEach(function (item){
     item.disabled=disabled;
   });
-  
+  document.getElementById('plain').innerHTML="";
 }
